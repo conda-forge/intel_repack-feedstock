1123105227a0c1f27160284cb4e6efc714c36ca5
Isuru Fernando <isuruf@gmail.com>
2020-11-10 11:40:50 -0600

    Update repack.bat

--- recipe/repack.bat
+++ recipe/repack.bat
@@ -1,5 +1,5 @@
 set "src=%SRC_DIR%\%PKG_NAME%"
-COPY %SRC_DIR%\mkl\info\licenses\license.txt" %SRC_DIR%
+COPY %SRC_DIR%\mkl-devel\info\licenses\license.txt" %SRC_DIR%
 robocopy /E "%src%" "%PREFIX%"
 if %ERRORLEVEL% GEQ 8 exit 1
 
