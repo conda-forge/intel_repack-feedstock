fcaef3e3e8bd6bbc74f72aa0cacb0f826cfd2417
Isuru Fernando <isuruf@gmail.com>
2021-03-31 08:39:47 -0500

    remove unnecessary license copying stuff

--- recipe/repack.sh
+++ recipe/repack.sh
@@ -5,12 +5,8 @@ set -ex
 #    That's what this $PKG_NAME is doing - picking the right subfolder to rsync
 
 src="$SRC_DIR/$PKG_NAME"
-# not all packages have the license file.  Copy it from mkl, where we know it exists
-cp -f "$SRC_DIR/mkl/info/licenses/license.txt" "$SRC_DIR"
-cp -rv "$src"/* "$PREFIX/"
 
-# ro by default.  Makes installations not cleanly removable.
-chmod 664 "$SRC_DIR/license.txt"
+cp -rv "$src"/* "$PREFIX/"
 
 # replace old info folder with our new regenerated one
 rm -rf "$PREFIX/info"
