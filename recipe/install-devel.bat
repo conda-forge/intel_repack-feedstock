f2a9fe0f0b78066f9c869182b66844472163c09e
Nikolay Petrov <nikolay.a.petrov@intel.com>
2023-09-22 10:11:09 +0100

    Enable pkg config for MKL devel package (#54)
    * Enabling pkgconfig for mkl-devel package
    
    * Bump version
    
    * MNT: Re-rendered with conda-build 3.26.1, conda-smithy 3.26.2, and conda-forge-pinning 2023.09.21.18.50.58
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/install-devel.bat
+++ recipe/install-devel.bat
@@ -9,5 +9,13 @@ echo include_dirs = %LIBRARY_INC%  >> %PREFIX%\site.cfg
 robocopy /E "%src%" "%PREFIX%"
 if %ERRORLEVEL% GEQ 8 exit 1
 
+:: correct .pc files that point to intel64 lib dir instead of just lib
+for /r "%PREFIX%\Library\lib\pkgconfig\" %%i in (*.pc) do (
+	sed -i.bak -E "s|libdir=(.*)/intel64|libdir=\1|g" "%%i"
+	if errorlevel 1 exit 1
+	del "%%i.bak"
+	type "%%i"
+)
+
 :: replace old info folder with our new regenerated one
 rd /s /q %PREFIX%\info
